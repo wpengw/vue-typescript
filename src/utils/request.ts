ee23c40adcc5becd65de3c3182a0ef7b170f7140
wpengw <wpengw@yeah.net>
2020-12-20 21:57:33 +0800

    'token'

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,11 +1,23 @@
 import axios from 'axios'
 import store from '@/store/index'
+import { Message } from 'element-ui'
+import router from '@/router'
+import qs from 'qs'
 
 const request = axios.create({
   // 配置选项
   // baseURL
   // timeout
 })
+
+function redirectLogin () {
+  router.push({
+    name: 'login',
+    query: {
+      redirect: router.currentRoute.fullPath
+    }
+  })
+}
 
 // 请求拦截器
 request.interceptors.request.use(function (config) {
@@ -17,6 +29,52 @@ request.interceptors.request.use(function (config) {
 }, function (error) {
   return Promise.reject(error)
 })
+
 // 响应拦截器
+request.interceptors.response.use(function (response) {
+  return response
+}, async function (error) {
+  // console.dir('response', error)
+  if (error.response) { // 请求发出去收到响应了，但状态码超出了 2xx范围
+    const { status } = error.response
+    if (status === 400) {
+      Message.error('请求参数错误')
+    } else if (status === 401) {
+      // token 无效 (没有提供 token, token 是无效的，token过期了)
+      // 如果没有用户信息
+      if (!store.state.user) {
+        redirectLogin()
+        return Promise.reject(error)
+      }
+      // 尝试刷新获取新的 token
+      try {
+        const { data } = await axios.create({
+          method: 'POST',
+          url: '/front/user/refresh_token',
+          data: qs.stringify({
+            refreshtoken: store.state.user.refresh_token
+          })
+        })
+        console.log(data)
+      } catch (err) {
+        redirectLogin()
+        return Promise.reject(err)
+      }
+    } else if (status === 403) {
+      Message.error('没有权限，请联系管理员')
+    } else if (status === 404) {
+      Message.error('请求资源不存在')
+    } else if (status >= 500) {
+      Message.error('服务端错误，请联系管理员')
+    }
+  } else if (error.request) { // 请求发出去没有收到响应
+    Message.error('请求超时，请刷新重试')
+  } else { // 在设置请求时发生了一些事情，触发了一个错误
+    Message.error(`请求失败：${error.message}`)
+  }
+
+  // 把请求失败的错误对象继续抛出，扔给上一个调用者
+  return Promise.reject(error)
+})
 
 export default request
